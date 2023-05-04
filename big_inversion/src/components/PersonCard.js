1b84fdee3d779f45f7598df4a4cdccd525261fff
Matthew Shoemaker <126444415+Lithelyne@users.noreply.github.com>
2023-05-04 09:16:24 -0700

    Add files via upload

---
+++ big_inversion/src/components/PersonCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const PersonCard = (props) => {
+    return (
+      <div>
+        <div>
+          <h1>{ props.lastName }, { props.firstName }</h1>
+          <p>Age: { props.age }</p>
+          <p>Hair Color: { props.hairColor }</p>
+        </div>
+      </div>
+    )
+  }
+
+export default PersonCard;
+
+
